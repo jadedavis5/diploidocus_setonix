7cd849cd4233aea928c8ba254822f01cc2b86b8f
jadedavis5 <111946376+jadedavis5@users.noreply.github.com>
2024-02-06 11:37:19 +0800

    Update slurm_diploidocus.sh

--- slurm_diploidocus.sh
+++ slurm_diploidocus.sh
@@ -12,6 +12,5 @@
 #SBATCH --export=ALL
 
 module load nextflow/23.10.0
-module load minimap2/2.26--he4a0461_1
 module load singularity/3.11.4-nompi
 nextflow run main.nf --projectDir . --assembly path/to/assembly/file -disable-jobs-cancellation
